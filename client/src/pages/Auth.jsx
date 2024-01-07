5a030f495b3070550a5a255484bf26ab97ed6f03
jasor63 <surhentharchess7gmail.com>
2024-01-07 22:58:28 +0530

    Connect Registration

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,19 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Auth() {
   return (
     <div>
-      <h3>Login</h3>
-      <form>
+      <Login />
+      <Register />
+    </div>
+  );
+}
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <Form
+      name="Login"
+      username={username}
+      setUsername={setUsername}
+      password={password}
+      setPassword={setPassword}
+    />
+  );
+};
+
+const Register = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      axios.post("http://localhost:3001/auth/register", {
+        username,
+        password,
+      });
+      alert("registration success");
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  return (
+    <Form
+      name="Register"
+      username={username}
+      setUsername={setUsername}
+      password={password}
+      setPassword={setPassword}
+      handleSubmit={handleSubmit}
+    />
+  );
+};
+
+function Form({
+  username,
+  setUsername,
+  password,
+  setPassword,
+  name,
+  handleSubmit,
+}) {
+  return (
+    <div>
+      <h3>{name}</h3>
+      <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="username">Username</label>
-          <input type="text" id="username" />
+          <label>
+            Username
+            <input
+              type="text"
+              name="username"
+              onChange={(event) => {
+                setUsername(event.target.value);
+              }}
+              value={username}
+            />
+          </label>
         </div>
         <div>
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" />
+          <label>
+            Password
+            <input
+              type="password"
+              name="password"
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+              value={password}
+            />
+          </label>
         </div>
-        <button>Submit</button>
+        <button>{name}</button>
       </form>
     </div>
   );
